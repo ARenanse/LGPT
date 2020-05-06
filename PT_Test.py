3f8bdeadec5f08c4d14b070f7627a1300d936134
WhenDustSettles <54006172+WhenDustSettles@users.noreply.github.com>
2020-05-07 02:16:39 +0530

    Update PT_Test.py

--- PT_Test.py
+++ PT_Test.py
@@ -3,6 +3,7 @@ import LGPT as pt
 
 # For importing custom model.
 from PTReplica import BasicModel
+import numpy as np
 import torch
 import time
 
@@ -11,14 +12,26 @@ import time
 
 if __name__ == "__main__":
 
-	Model = pt.ParallelTempering(BasicModel, 5, 100000, 0.2, 5000, 'GEO')
+	x = np.random.uniform(-50,50,size = [1000,1])
+	y = np.sin(x) /x
+	train = torch.tensor(np.concatenate( (x,y) ,axis = 1), dtype = torch.float)
+
+	x_test = np.random.uniform(-50,50,size = [100,1])
+	y_test = np.sin(x_test) / x_test
+	test = torch.tensor(np.concatenate((x_test,y_test),axis = 1), dtype = torch.float)
+
+
+
+	Model = pt.ParallelTempering(BasicModel, 15, 100000, 200, 5000, 'GEO')
 
 	time.sleep(2)
 
-	train = torch.tensor([[1,2,3,10],[4,5,6,10],[7,8,9,10],[11,12,13,10],[14,15,16,10]], dtype = torch.float)
-	test = torch.tensor([[17,18,19,10],[20,21,22,10],[23,24,25,10]], dtype = torch.float)
+	# train = torch.tensor([[1,2,3,10],[4,5,6,10],[7,8,9,10],[11,12,13,10],[14,15,16,10]], dtype = torch.float)
+	# test = torch.tensor([[17,18,19,10],[20,21,22,10],[23,24,25,10]], dtype = torch.float)
 
 	#                         DNM     DNM                                    
-	Model.InitReplicas(3,5,1, 200,0.6,700,True,0.5,train,test,0.001,0.025, "ANYTHING")
+	Model.InitReplicas(1,5,1, 200,0.6,700,True,0.5,train,test,0.001,0.05, "ANYTHING")
 
-	Model.RunChains()
+	np.save('test_set.npy', np.array(test), allow_pickle = True)
+
+	Model.RunChains(SamplesFileName = 'Samples2.npy')
